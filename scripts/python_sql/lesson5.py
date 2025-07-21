9059668b5b6f7ca0d54144c9a1401d22ac6e8825
alex <berezov86@gmail.com>
2025-07-21 17:14:01 +0300

    update lesson 5

--- scripts/python_sql/lesson5.py
+++ scripts/python_sql/lesson5.py
@@ -1,31 +1,21 @@
 from db_connection import get_connection
-conn = get_connection()
 
 
-def get_order_sum(conn, month):
-    query = """SELECT
-                customer_name,
-                EXTRACT(MONTH FROM order_date) as month,
-                SUM(total_amount)
-            FROM orders AS o
-            JOIN customers AS c
-            ON o.customer_id = c.customer_id
-            GROUP BY customer_name, order_date
-            HAVING EXTRACT(MONTH FROM order_date) = %s;"""
-    result = []
+def get_error():    
+    conn = get_connection()
+    query_select = """SELECT * FROM users;"""
+    query_insert = """INSERT INTO users (username, phone) VALUES ('tommy2', '12223456789');"""
 
-    with conn.cursor(cursor_factory=DictCursor) as cur:
-        cur.execute(query, (month,))
-        rows = cur.fetchall()
-        for row in rows:
-            text = f'Покупатель {row[0]} совершил покупок на сумму {row[2]}'
-            result.append(text)
-    result = '\n'.join(result)
+    cursor = conn.cursor()
+    cursor.execute(query_select)
+    rows = cursor.fetchall()
+    print(rows)
+    cursor.execute(query_insert)
+    
+    raise Exception("Ошибка во время транзакции")
+    
+    conn.commit()
+    conn.close()
 
-    print(result)
-    return result
-
-# end
-get_order_sum(conn, 2)
-conn.close()
+get_error()
 
